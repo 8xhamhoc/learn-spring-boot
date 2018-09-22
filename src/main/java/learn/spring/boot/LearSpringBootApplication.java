5a51372453105982d3acd95996e72f4aa185545e
8xquang <quang.phan@smartdev.vn>
2018-09-22 15:35:00 +0700

    init dev

--- src/main/java/learn/spring/boot/LearSpringBootApplication.java
+++ src/main/java/learn/spring/boot/LearSpringBootApplication.java
@@ -9,4 +9,5 @@ public class LearSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearSpringBootApplication.class, args);
 	}
+
 }
---
+++ src/main/java/learn/spring/boot/StartApp.java
@@ -0,0 +1,28 @@
+package learn.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StartApp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class, args);
+    }
+
+    @RequestMapping("/")
+    public String hell() {
+        return "Hello";
+    }
+
+}
