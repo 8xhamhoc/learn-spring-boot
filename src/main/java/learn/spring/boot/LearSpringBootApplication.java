d9946132db569ff29367114087c605c68a90e623
8xquang <quang.phan@smartdev.vn>
2018-09-22 10:52:15 +0700

    first commit

---
+++ src/main/java/learn/spring/boot/LearSpringBootApplication.java
@@ -0,0 +1,12 @@
+package learn.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearSpringBootApplication.class, args);
+	}
+}
