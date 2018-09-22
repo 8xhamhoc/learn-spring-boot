c15487c44c2e11a9a2b3234ef13593f34c3a008b
8xquang <quang.phan@smartdev.vn>
2018-09-22 18:18:39 +0700

    spring-boot-interceptor: create class

---
+++ src/main/java/learn/spring/boot/controller/InterceptorController.java
@@ -0,0 +1,7 @@
+package learn.spring.boot.controller;
+
+/**
+ * Created by quangphan on 22/09/2018.
+ */
+public class InterceptorController {
+}
---
+++ src/main/java/learn/spring/boot/interceptor/ProductServiceInterceptor.java
@@ -0,0 +1,28 @@
+package learn.spring.boot.interceptor;
+
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class ProductServiceInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+
+    }
+}
