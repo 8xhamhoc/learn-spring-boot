f2eab79364e58ab2cfab51568c4e6cad17b4174f
8xhamhoc <tomatoquang@gmail.com>
2018-09-27 22:57:00 +0700

    spring-boot profiles init

--- src/main/java/learn/spring/boot/controller/InterceptorController.java
+++ src/main/java/learn/spring/boot/controller/InterceptorController.java
@@ -1,6 +1,9 @@
 package learn.spring.boot.controller;
 
 import learn.spring.boot.model.Product;
+import learn.spring.boot.property.Global;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,13 @@ import java.util.Map;
 
 @RestController
 public class InterceptorController {
+
+    @Autowired
+    private Global global;
+    @Value("${global.name}")
+    private String globalName;
+    @Value("${global.age}")
+    private int globalAge;
 
     private static Map<String, Product> productRepo = new HashMap<>();
 
@@ -27,6 +37,8 @@ public class InterceptorController {
 
     @RequestMapping(value = "/products")
     public ResponseEntity<Object> getProduct() {
+        System.out.println("Name: " + globalName);
+        System.out.println("Age: " + globalAge);
         return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
     }
 
---
+++ src/main/java/learn/spring/boot/property/Global.java
@@ -0,0 +1,32 @@
+package learn.spring.boot.property;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:global.properties")
+public class Global {
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
